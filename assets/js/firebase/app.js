f05f73568621d16867d8dd3457ca67f6c81df461
Anderson <andersonffilho2002@gmail.com>
2022-08-27 12:14:20 -0300

    js

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAme0ogSAdo8QC0uH_OKTS_r9FDlDPyDOw",
+  authDomain: "mundo-invertido-798df.firebaseapp.com",
+  projectId: "mundo-invertido-798df",
+  storageBucket: "mundo-invertido-798df.appspot.com",
+  messagingSenderId: "519929557176",
+  appId: "1:519929557176:web:2f60096d74b423ca24444d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+export default app
